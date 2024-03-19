83765f8a42d3b8091805a7fb9b777e21f268a040
SHARAD <sharadt2403@gmail.com>
2024-03-19 13:59:51 +0000

    added content in testingGit.js

---
+++ src/components/testingGit.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+
+class TestingGit extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    )
+  }
+}
+
+export default TestingGit
