17964ca985f89980b52fbc1d745ec921b8244c25
SHARAD <sharadt2403@gmail.com>
2024-05-09 14:16:00 +0000

    09 May, Modified Header for mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import {Link, withRouter} from 'react-router-dom'
 import Cookies from 'js-cookie'
 import './Header.css'
+import {TiHome} from 'react-icons/ti'
+import {FaSearch} from 'react-icons/fa'
+import {FiLogOut} from 'react-icons/fi'
 
 const Header = props => {
   const {history} = props
@@ -11,28 +14,62 @@ const Header = props => {
   }
 
   return (
-    <div className="header-container">
-      <Link to="/">
-        <img
-          className="logo-image"
-          src="https://assets.ccbp.in/frontend/react-js/logo-img.png "
-          alt="website logo"
-        />
-      </Link>
-      <ul className="list-container">
-        <div className="options-container">
-          <Link to="/" className="link">
-            <li className="options">Home</li>
-          </Link>
-          <Link to="/jobs" className="link">
-            <li className="options">Jobs</li>
-          </Link>
-        </div>
-        <button className="logout-button" type="button" onClick={logout}>
-          Logout
-        </button>
-      </ul>
-    </div>
+    <>
+      <div className="header-container">
+        <Link to="/">
+          <img
+            className="logo-image"
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png "
+            alt="website logo"
+          />
+        </Link>
+        <ul className="list-container">
+          <div className="options-container">
+            <Link to="/" className="link">
+              <li className="options">Home</li>
+            </Link>
+            <Link to="/jobs" className="link">
+              <li className="options">Jobs</li>
+            </Link>
+          </div>
+          <button className="logout-button" type="button" onClick={logout}>
+            Logout
+          </button>
+        </ul>
+      </div>
+
+      <div className="header-container-mobile">
+        <Link to="/" className="link-image-mobile">
+          <img
+            className="logo-image-mobile"
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png "
+            alt="website logo"
+          />
+        </Link>
+        <ul className="list-container-mobile">
+          <div className="options-container-mobile">
+            <Link to="/" className="link-mobile">
+              <li className="options-mobile">
+                <TiHome className="icon-home-mobile" />
+              </li>
+            </Link>
+            <Link to="/jobs" className="link-mobile">
+              <li className="options-mobile">
+                <FaSearch className="icon-search-mobile" />
+              </li>
+            </Link>
+          </div>
+          {/* eslint-disable-next-line */}
+          <button
+            className="logout-button-mobile"
+            type="button"
+            onClick={logout}
+          >
+            <FiLogOut />
+          </button>
+        </ul>
+      </div>
+    </>
   )
 }
 
